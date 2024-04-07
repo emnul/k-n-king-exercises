6497936e3d2f54e9160c2e1898fdf6cbbc99f7af
emanuel-sol <esolis6114@gmail.com>
2024-04-06 22:26:15 -0400

    feat(/pun_c): Add more details to program

---
+++ pun_c/README.md
@@ -0,0 +1,1 @@
+pun.c is an exercise in identifying the core structures of every c program
--- pun_c/pun.c
+++ pun_c/pun.c
@@ -4,7 +4,8 @@
 // is to be "included" into the program BEFORE it's compiled
 #include <stdio.h>
 
-int main(void) {
-  printf("To C, or not to C: that is the question.\n");
-  return 0;
+int main(void) { // main is a special function that's required in all c programs
+  printf(
+      "To C, or not to C: that is the question.\n"); // funciton call statement
+  return 0; // return statement; indicates main terminated successfully
 }
