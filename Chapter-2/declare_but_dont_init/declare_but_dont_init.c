af107bc6de712a9ffb176994aa71adf897255b3f
emanuel-sol <esolis6114@gmail.com>
2024-04-07 19:03:32 -0400

    refactor(**/*): Restructure project

---
+++ Chapter-2/declare_but_dont_init/declare_but_dont_init.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+
+int main(void)
+{
+   int a, b, c;
+   float d, e, f;
+
+   printf("a is: %d\nb is: %d\nc is: %d\nd is: %.2f\ne is %.2f\nf is: %.2f\n",
+          a, b, c, d, e, f);
+}
