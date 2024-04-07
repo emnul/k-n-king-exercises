85b1863c93420c13fdf99f614262433b0f15139b
emanuel-sol <esolis6114@gmail.com>
2024-04-06 22:43:45 -0400

    style(pun.c): formatting changes

--- pun_c/pun.c
+++ pun_c/pun.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 int main(void) { // main is a special function that's required in all c programs
-  printf(
-      "To C, or not to C: that is the question.\n"); // funciton call statement
-  return 0; // return statement; indicates main terminated successfully
+   printf(
+         "To C, or not to C: that is the question.\n"); // funciton call statement
+   return 0; // return statement; indicates main terminated successfully
 }
