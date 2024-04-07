bef17312eb57672fc63dc4248931bdcd816e799a
emanuel-sol <esolis6114@gmail.com>
2024-04-06 22:12:54 -0400

    feat(pun_c/): My first c program

---
+++ pun_c/pun.c
@@ -0,0 +1,10 @@
+// example of a directive
+// directives are commands intended for the preprocessor
+// directives state that the information in <stdio.h>
+// is to be "included" into the program BEFORE it's compiled
+#include <stdio.h>
+
+int main(void) {
+  printf("To C, or not to C: that is the question.\n");
+  return 0;
+}
